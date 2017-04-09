029e7042b438cbf4cdd572e0581e85fac1a9bc8d
Mel Huang <hello@melaniehuang.com>
2017-04-09 16:33:25 +1000

    caption arrow through implemented with pauses

--- scripts/caption.js
+++ scripts/caption.js
@@ -1,2 +1,39 @@
-var line = { "name": "Hexate", "line": "Ira [chanting]" };
-console.log(line.name, line.line);
+var lines;
+var lineNum;
+
+function preload() {
+  var request = "/subtitles/captions-manual.json";
+  lines = loadJSON(request); 
+}
+
+function setup(){
+  lineNum = -1;
+}
+
+function draw(){
+
+}
+
+function keyPressed(){
+  if (lineNum => -1 && lineNum < lines.captions.size -1){
+
+	  if (keyCode === RIGHT_ARROW) {
+	    lineNum++;
+	  } else if (keyCode === LEFT_ARROW) {
+	  	lineNum--;
+	  }
+	  
+	  var character = lines.captions[lineNum].name;
+	  var speech = lines.captions[lineNum].line;
+
+	  if (character == ""){
+	    document.getElementById("demo").innerHTML =  speech; 
+	  } else {
+	    document.getElementById("demo").innerHTML = "[ " + character + " ]: " + speech; 
+	  } 
+
+	  if (keyCode === DOWN_ARROW) {
+	  	document.getElementById("demo").innerHTML =  ""; 
+	  }
+  }
+}
